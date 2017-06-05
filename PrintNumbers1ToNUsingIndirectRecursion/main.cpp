34784301e90bd5b450e1bac8988365a41eae6da0
thanhtungpfiev <thanhtungpfiev@gmail.com>
2017-06-05 09:10:35 +0700

    PrintNumbers1ToNUsingIndirectRecursion: using static instance variable

--- PrintNumbers1ToNUsingIndirectRecursion/main.cpp
+++ PrintNumbers1ToNUsingIndirectRecursion/main.cpp
@@ -12,30 +12,23 @@
 
 using namespace std;
 
-template<int N>
 class PrintOneToN
 {
 public:
-    static void print()
+    PrintOneToN()
     {
-        PrintOneToN<N-1>::print();
-        cout << N << endl;
+        cout << count++ << endl;
     }
+private:
+    static int count;
+
 };
 
-template<>
-class PrintOneToN<1>
-{
-public:
-    static void print()
-    {
-        cout << 1 << endl;
-    }
-};
+int PrintOneToN::count = 1;
 
 int main(int argc, char *argv[])
 {
     const int N = 20;
-    PrintOneToN<N>::print();
+    PrintOneToN printOneToN[N];
     return 0;
 }
